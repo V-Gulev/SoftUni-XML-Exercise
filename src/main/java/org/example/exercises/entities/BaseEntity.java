bb1b45cc4c8d2201a1f2db2c660f5ebf896bbca3
V-Gulev <velizargulev@abv.bg>
2025-08-01 13:57:17 +0300

    XML Exercises

---
+++ src/main/java/org/example/exercises/entities/BaseEntity.java
@@ -0,0 +1,18 @@
+package org.example.exercises.entities;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
