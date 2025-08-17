ca8877fa1be0876979cd6bd9290de5fb2370fb46
V-Gulev <velizargulev@abv.bg>
2025-08-17 21:59:53 +0300

    XML Exercises PartRepository

---
+++ src/main/java/org/example/exercises/repositories/PartRepository.java
@@ -0,0 +1,9 @@
+package org.example.exercises.repositories;
+
+import org.example.exercises.entities.Part;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartRepository extends JpaRepository<Part, Long> {
+}
