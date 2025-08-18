7306ae99e182fb757a5c59624863dbc43d5bc9b2
V-Gulev <velizargulev@abv.bg>
2025-08-18 21:24:36 +0300

    XML Exercises SupplierDto

---
+++ src/main/java/org/example/exercises/dtos/SupplierInputDto.java
@@ -0,0 +1,30 @@
+package org.example.exercises.dtos;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SupplierInputDto {
+    @XmlAttribute(name = "name")
+    private String name;
+
+    @XmlAttribute(name = "is-importer")
+    private boolean isImporter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isImporter() {
+        return isImporter;
+    }
+
+    public void setImporter(boolean importer) {
+        isImporter = importer;
+    }
+}
