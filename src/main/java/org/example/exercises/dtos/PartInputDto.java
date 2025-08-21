292fbff1e77d9db77b16813ac1348e48e15bf312
V-Gulev <velizargulev@abv.bg>
2025-08-21 19:30:08 +0300

    XML Exercises PartInputDto

---
+++ src/main/java/org/example/exercises/dtos/PartInputDto.java
@@ -0,0 +1,43 @@
+package org.example.exercises.dtos;
+
+import jakarta.xml.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PartInputDto {
+    @XmlAttribute(name = "name")
+    private String name;
+
+    @XmlAttribute(name = "price")
+    private BigDecimal price;
+
+    @XmlAttribute(name = "quantity")
+    private Long quantity;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+}
