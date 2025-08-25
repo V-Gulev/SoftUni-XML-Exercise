9281eff0516b3d3221dbb483c993df15df282f41
V-Gulev <velizargulev@abv.bg>
2025-08-25 20:52:58 +0300

    XML Exercises CarDto

---
+++ src/main/java/org/example/exercises/dtos/CarsImportDto.java
@@ -0,0 +1,25 @@
+package org.example.exercises.dtos;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.util.List;
+
+@XmlRootElement(name = "cars")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CarsImportDto {
+
+    @XmlElement(name = "car")
+    private List<CarInputDto> cars;
+
+    public List<CarInputDto> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<CarInputDto> cars) {
+        this.cars = cars;
+    }
+
+}
